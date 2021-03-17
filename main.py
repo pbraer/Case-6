994df18f3d04993f323073239a83480617021a09
pbraer <73088589+pbraer@users.noreply.github.com>
2021-03-17 09:30:17 +0700

    head

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print('Hello')
+# Case-study #6
+# Developers:   Braer P. (%),
+#               Kokorina D. (%),
+#               Novoselov V. (%)
+
+print("""Case-study Тесселяция
+Разработчики:
+Браер П.С., Кокорина Д.Е., Новоселов В.В.
+
+""")
+
